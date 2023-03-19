f517a883f09e4090981b250cbb728b485160353a
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-19 01:46:47 +0000

    update

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ elif page == "Channel Details":
         if channel_choice == stats_df["channelName"][0]:
             fig = px.bar(data_frame=co_df.sort_values('viewCount', ascending=False)[0:9],
                           x="title", y="viewCount", color='viewCount', title="Best Performing Videos")
-            fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
+            fig.update_layout(margin=dict(l=40, r=30, t=20, b=20))
         else:
             fig = px.bar(data_frame=co_df.sort_values('viewCount', ascending=False),
                           x="title", y="likeCount", color='viewCount', title="Subscribers-Total Videos")
