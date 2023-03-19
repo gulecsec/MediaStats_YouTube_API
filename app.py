492e1672588b777377470e450f2fe9cb7305f17f
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-19 02:10:50 +0000

    update

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ elif page == "Channel Details":
 
         # display the selected graph
         if channel_choice == stats_df["channelName"][0]:
-            fig1 = px.bar(data_frame=co_df.sort_values('likeCount', ascending=True)[0:9],
-                          x="title", y="likeCount", color='viewCount', title="likeCount-viewCount")
+            fig1 = px.bar(data_frame=co_df.sort_values('likeCount', ascending=False)[0:9],
+                          x="title", y="likeCount", color='likeCount', title="likeCount-viewCount")
             st.plotly_chart(fig1)
         else:
             fig1 = px.bar(data_frame=co_df.sort_values('likeCount', ascending=False),
