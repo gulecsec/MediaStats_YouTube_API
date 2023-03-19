d20f721bdfe199bf1f07ad965e5842b24d3b6e81
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-19 00:32:11 +0000

    update

--- app.py
+++ app.py
@@ -6,36 +6,77 @@ import plotly.express as px
 # Load Data
 stats_df = pd.read_csv("media_stats/media_stats.csv")
 
+# Define page layout
 header = st.container()
 dataset= st.container()
 features = st.container()
 
-with header:
-    st.title("How Turkish News Media's YouTube Stats Stack Up: Exploring the Data on My Streamlit App")
-    st.markdown("In this tutorial, we'll explore how to use the YouTube API with Python to interact with one of the world's largest video-sharing platforms,which boasts billions of daily users uploading, viewing, and commenting on videos. By the end of this tutorial, you'll know how to automate various tasks using the YouTube API.")
+# Define navigation items
+nav_items = ["All Channels"] + list(stats_df["channelName"].unique())
 
-with features:
-    st.header("")
-    st.text("")
-
-with dataset:
-    st.subheader("YouTube Stats of Turkish News Media in YouTube")
-    years=[ i for i in range(2010, 2024)]
-
-    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-    channels = ["Cuneyt Ozdemir Media", "AHaber"]
-
-    # add radio button to select between the two graphs
-    graph_choice = st.radio("Select graph", options=["Views-Total Videos", "Subscribers-Total Videos"])
-
-    # display the selected graph
-    if graph_choice == "Views-Total Videos":
-        fig = px.bar(data_frame=stats_df.sort_values('views', ascending=False),
-                      x="channelName", y="views",color='totalVideos', title="Views-Total Videos")
+# Define helper function to get channel data
+def get_channel_data(channel_name):
+    if channel_name == "All Channels":
+        return stats_df
     else:
-        fig = px.bar(data_frame=stats_df.sort_values('subscribers', ascending=False),
-                      x="channelName", y="subscribers",color='totalVideos', title="Subscribers-Total Videos")
-    st.plotly_chart(fig)
+        return stats_df[stats_df["channelName"] == channel_name]
+
+# Define pages
+def show_homepage():
+    with header:
+        st.title("How Turkish News Media's YouTube Stats Stack Up: Exploring the Data on My Streamlit App")
+        st.markdown("In this tutorial, we'll explore how to use the YouTube API with Python to interact with one of the world's largest video-sharing platforms, which boasts billions of daily users uploading, viewing, and commenting on videos. By the end of this tutorial, you'll know how to automate various tasks using the YouTube API.")
+
+    with navigation:
+        st.sidebar.title("Navigation")
+        selection = st.sidebar.radio("Go to", nav_items)
+
+    with channel_stats:
+        if selection == "All Channels":
+            fig1 = px.bar(data_frame=stats_df.sort_values('views', ascending=False),
+                          x="channelName", y="views",color='totalVideos', title="Views-Total Videos")
+            st.write(fig1)
+
+            fig2 = px.bar(data_frame=stats_df.sort_values('subscribers', ascending=False),
+                          x="channelName", y="subscribers",color='totalVideos', title="Subscribers-Total Videos")
+            st.write(fig2)
+        else:
+            channel_data = get_channel_data(selection)
+
+            fig1 = px.bar(data_frame=channel_data.sort_values('views', ascending=False),
+                          x="month", y="views",color='totalVideos', title=f"{selection} Views-Total Videos")
+            st.write(fig1)
+
+            fig2 = px.bar(data_frame=channel_data.sort_values('subscribers', ascending=False),
+                          x="month", y="subscribers",color='totalVideos', title=f"{selection} Subscribers-Total Videos")
+            st.write(fig2)
+
+def show_channel_stats():
+    with header:
+        st.title("Channel Stats")
+
+    with navigation:
+        st.sidebar.title("Navigation")
+        selection = st.sidebar.radio("Go to", nav_items)
+
+    with channel_stats:
+        channel_data = get_channel_data(selection)
+
+        fig1 = px.bar(data_frame=channel_data.sort_values('views', ascending=False),
+                      x="month", y="views",color='totalVideos', title=f"{selection} Views-Total Videos")
+        st.write(fig1)
+
+        fig2 = px.bar(data_frame=channel_data.sort_values('subscribers', ascending=False),
+                      x="month", y="subscribers",color='totalVideos', title=f"{selection} Subscribers-Total Videos")
+        st.write(fig2)
+
+# Run app
+if __name__ == "__main__":
+    page = st.sidebar.selectbox("Select a page", ["Homepage", "Channel Stats"])
+    if page == "Homepage":
+        show_homepage()
+    elif page == "Channel Stats":
+        show_channel_stats()
 
     # # selected_media =
     # selected_year = st.sidebar.selectbox("Select Year", [""] + years, index=0)
