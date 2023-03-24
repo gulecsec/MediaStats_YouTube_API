57a5ea73274658ba57268f20e420402982131c3a
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-24 23:34:09 +0000

    update

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ if page == "Video Stats After 6th of Feb 2023":
             # generate a horizontal bar chart using Plotly
             fig = px.bar(view_count_df, barmode='group', title="Views-Video Durations After 6th of Feb")
 
-            fig.update_layout(coloraxis_colorbar=dict(title="Total Videos","s"))
+            fig.update_layout(coloraxis_colorbar=dict(title=["Total Videos","s"]))
 
             fig.update_layout(xaxis_title=None)
             fig.update_layout(yaxis_title=None)
