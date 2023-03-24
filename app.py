aee46216cea0f500e74dd4ba556656b3da3aa649
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-24 23:51:54 +0000

    update

--- app.py
+++ app.py
@@ -205,12 +205,9 @@ if page == "Turkish News Media's YouTube Stats":
 
             fig.update_traces(name="Likes",selector=dict(name="like_count_after"))
 
-            fig.update_layout(legend=dict(orientation='h',yanchor='top',y=1.1,xanchor='left',x=0.01),legend_title="")
-
-            fig.update_layout(
-                width=800,  # set the width to 800 pixels
-                height=600, # set the height to 600 pixels
-            )
+            fig.update_layout(legend=dict(orientation='h',yanchor='top',y=1.1,xanchor='left',x=0.01),legend_title="",
+            width=800,  # set the width to 800 pixels
+            height=600, # set the height to 600 pixels)
 
             # display the chart
             st.plotly_chart(fig)
