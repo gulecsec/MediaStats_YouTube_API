f74b20de111c9c3d83b947c7ed8fcef013565230
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-25 01:01:39 +0000

    update

--- app.py
+++ app.py
@@ -440,22 +440,22 @@ elif page == "Top 10 Videos by Like Count and View Count":
     with channel_details:
         st.title("Channel Details")
 
-        # Load Each Channel Data
-        ahaber_df = pd.read_csv("media_stats/stats_a_haber.csv")
-        aa_df = pd.read_csv("media_stats/stats_anadolu_ajansı.csv")
-        bab_df = pd.read_csv("media_stats/stats_babala_tv.csv")
-        bbc_df = pd.read_csv("media_stats/stats_bbc_news_türkçe.csv")
-        cnn_df = pd.read_csv("media_stats/stats_cnn_türk.csv")
-        co_df = pd.read_csv("media_stats/stats_cüneyt_özdemir.csv")
-        co_df = pd.read_csv("media_stats/stats_erk_acarer.csv")
-        fox_df = pd.read_csv("media_stats/stats_fox_haber.csv")
-        ht_df = pd.read_csv("media_stats/stats_habertürk.csv")
-        h_tv_df = pd.read_csv("media_stats/stats_halktv.csv")
-        nev_df = pd.read_csv("media_stats/stats_nevşin_mengü.csv")
-        soz_df = pd.read_csv("media_stats/stats_sözcü_televizyonu.csv")
-        trt_df = pd.read_csv("media_stats/stats_trt_haber.csv")
-        tv_df = pd.read_csv("media_stats/stats_tv100.csv")
-        ys_df = pd.read_csv("media_stats/stats_yeni_şafak.csv")
+        # # Load Each Channel Data
+        # ahaber_df = pd.read_csv("media_stats/stats_a_haber.csv")
+        # aa_df = pd.read_csv("media_stats/stats_anadolu_ajansı.csv")
+        # bab_df = pd.read_csv("media_stats/stats_babala_tv.csv")
+        # bbc_df = pd.read_csv("media_stats/stats_bbc_news_türkçe.csv")
+        # cnn_df = pd.read_csv("media_stats/stats_cnn_türk.csv")
+        # co_df = pd.read_csv("media_stats/stats_cüneyt_özdemir.csv")
+        # co_df = pd.read_csv("media_stats/stats_erk_acarer.csv")
+        # fox_df = pd.read_csv("media_stats/stats_fox_haber.csv")
+        # ht_df = pd.read_csv("media_stats/stats_habertürk.csv")
+        # h_tv_df = pd.read_csv("media_stats/stats_halktv.csv")
+        # nev_df = pd.read_csv("media_stats/stats_nevşin_mengü.csv")
+        # soz_df = pd.read_csv("media_stats/stats_sözcü_televizyonu.csv")
+        # trt_df = pd.read_csv("media_stats/stats_trt_haber.csv")
+        # tv_df = pd.read_csv("media_stats/stats_tv100.csv")
+        # ys_df = pd.read_csv("media_stats/stats_yeni_şafak.csv")
 
 
         # create a dictionary to store data frames and graph titles for each channel
@@ -467,15 +467,14 @@ elif page == "Top 10 Videos by Like Count and View Count":
         'CNN TÜRK': {'path': 'media_stats/stats_cnn_türk.csv' , 'title': 'CNN TÜRK Top 10 Videos by Like Count and View Count'},
         'Cüneyt Özdemir': {'path': 'media_stats/stats_cüneyt_özdemir.csv' , 'title': 'Cüneyt Özdemir Top 10 Videos by Like Count and View Count'},
         'Erk Acarer': {'path': 'media_stats/stats_cüneyt_özdemir.csv' , 'title': 'Cüneyt Özdemir Top 10 Videos by Like Count and View Count'},
-
-        'FOX Haber': {'df': fox_df,'path': 'media_stats/stats_anadolu_ajansı.csv' , 'title': 'FOX Haber Top 10 Videos by Like Count and View Count'},
-        'Habertürk': {'df': ht_df,'path': 'media_stats/stats_anadolu_ajansı.csv' , 'title': 'Habertürk Top 10 Videos by Like Count and View Count'},
-        'Halktv': {'df': h_tv_df,'path': 'media_stats/stats_anadolu_ajansı.csv' , 'title': 'Halktv Top 10 Videos by Like Count and View Count'},
-        'Nevşin Mengü': {'df': nev_df, 'path': 'media_stats/stats_anadolu_ajansı.csv' ,'title': 'Nevşin Mengü Top 10 Videos by Like Count and View Count'},
-        'SÖZCÜ Televizyonu': {'df': soz_df, 'path': 'media_stats/stats_anadolu_ajansı.csv' ,'title': 'SÖZCÜ Televizyonu Top 10 Videos by Like Count and View Count'},
-        'TRT Haber': {'df': trt_df, 'path': 'media_stats/stats_anadolu_ajansı.csv' ,'title': 'TRT Haber Top 10 Videos by Like Count and View Count'},
-        'TV100': {'df': tv_df, 'path': 'media_stats/stats_anadolu_ajansı.csv' ,'title': 'TV100 Top 10 Videos by Like Count and View Count'},
-        'Yeni Şafak': {'df': ys_df, 'path': 'media_stats/stats_anadolu_ajansı.csv' ,'title': 'Yeni Şafak Top 10 Videos by Like Count and View Count'},
+        'FOX Haber': {'df': fox_df,'path': 'media_stats/stats_fox_haber.csv' , 'title': 'FOX Haber Top 10 Videos by Like Count and View Count'},
+        'Habertürk': {'df': ht_df,'path': 'media_stats/stats_habertürk.csv' , 'title': 'Habertürk Top 10 Videos by Like Count and View Count'},
+        'Halktv': {'df': h_tv_df,'path': 'media_stats/stats_halktv.csv' , 'title': 'Halktv Top 10 Videos by Like Count and View Count'},
+        'Nevşin Mengü': {'df': nev_df, 'path': 'media_stats/stats_nevşin_mengü.csv' ,'title': 'Nevşin Mengü Top 10 Videos by Like Count and View Count'},
+        'SÖZCÜ Televizyonu': {'df': soz_df, 'path': 'media_stats/stats_sözcü_televizyonu.csv' ,'title': 'SÖZCÜ Televizyonu Top 10 Videos by Like Count and View Count'},
+        'TRT Haber': {'df': trt_df, 'path': 'media_stats/stats_trt_haber.csv' ,'title': 'TRT Haber Top 10 Videos by Like Count and View Count'},
+        'TV100': {'df': tv_df, 'path': 'media_stats/stats_tv100.csv' ,'title': 'TV100 Top 10 Videos by Like Count and View Count'},
+        'Yeni Şafak': {'df': ys_df, 'path': 'media_stats/stats_yeni_şafak.csv' ,'title': 'Yeni Şafak Top 10 Videos by Like Count and View Count'},
         }
 
 
