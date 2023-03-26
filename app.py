14e1e8c4000173cae2a3d75914377e58407e36a6
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-26 19:08:57 +0000

    update

--- app.py
+++ app.py
@@ -455,6 +455,25 @@ Additionally, the difference between the highest and lowest values for each colu
 
                 Looking at the data, we can see that some channels have had significant changes in their video count.
 
+Here are the percentage changes for each channel:
+
+Nevşin Mengü: 7.08%
+BaBaLa TV: 1.14%
+Cüneyt Özdemir: 7.55%
+TRT Haber: 2.83%
+BBC News Türkçe: 0.51%
+Yeni Şafak: 4.79%
+A Haber: 3.73%
+FOX Haber: 2.39%
+Habertürk: 1.59%
+TV100: 6.59%
+Anadolu Ajansı: 4.19%
+CNN TÜRK: 4.59%
+Halktv: 2.03%
+Erk Acarer: 7.25%
+
+These percentages represent the increase or decrease in the number of videos uploaded by each channel between the "video_count_before" and "video_count_after" columns.
+
 Overall, it seems that the majority of channels have had a relatively small change in their video count, with most falling within a range of 1-5% increase or decrease.
 
 However, it's important to note that these changes may not necessarily be significant in the context of each channel's total video count. For example, a 1% increase for a channel with only a few videos may be less significant than a 1% increase for a channel with thousands of videos.
