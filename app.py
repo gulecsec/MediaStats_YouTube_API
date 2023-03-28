f99d321ef6486141bb91f2d611328bd6aef71c68
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-28 03:39:19 +0000

    update

--- app.py
+++ app.py
@@ -163,10 +163,13 @@ if page == "Turkish News Media's YouTube Stats":
                 view_count_df = view_count_df.set_index('channelName')
 
                 # generate a horizontal bar chart using Plotly
-                fig = px.bar(view_count_df, x='view_count_after', y=view_count_df.index, color='mins_count_after',orientation='h', title="Total Video Minutes - Channel Views After 6th of Feb",coloraxis_colorbar=dict(title="Minutes"))
+                fig = px.bar(view_count_df, x='view_count_after', y=view_count_df.index,
+                color='mins_count_after',orientation='h',
+                title="Total Video Minutes - Channel Views After 6th of Feb")
 
                 fig.update_layout(xaxis_title=None, legend=dict(orientation='h', yanchor='top', y=1.1,
-                xanchor='left', x=0.01), legend_title="Minutes",width=800, height=600, yaxis_title=None)
+                xanchor='left', x=0.01), legend_title="Minutes",width=800, height=600, yaxis_title=None,
+                coloraxis_colorbar=dict(title="Minutes"))
 
                 # # generate a horizontal bar chart using Plotly
                 # fig = px.bar(view_count_df, barmode='group', title="Total Video Minutes - Channel Views After 6th of Feb")
