8adefff019014faab3a511fa3a30e3c6945ca3fd
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-28 03:31:08 +0000

    update

--- app.py
+++ app.py
@@ -160,17 +160,28 @@ if page == "Turkish News Media's YouTube Stats":
                 view_count_df = view_count_df.set_index('channelName')
 
                 # generate a horizontal bar chart using Plotly
-                fig = px.bar(view_count_df, barmode='group', title="Total Video Minutes - Channel Views After 6th of Feb")
+                fig = px.bar(view_count_df, x='mins_count_after', y=view_count_df.index, color='mins_count_after',
+                orientation='h', title="Total Video Minutes - Channel Views After 6th of Feb")
 
-                fig.update_traces(name="Minutes",selector=dict(name="mins_count_after"))
+                fig.update_layout(xaxis_title=None, legend=dict(orientation='h', yanchor='top', y=1.1,
+                                                  xanchor='left', x=0.01), legend_title="",
+                  width=800, height=600, yaxis_title=None)
 
-                fig.update_traces(name="Views",selector=dict(name="view_count_after"))
 
-                fig.update_layout(xaxis_title=None,legend=dict(orientation='h',yanchor='top',y=1.1,xanchor='left',x=0.01),legend_title="",
-                width=800, height=600,yaxis_title=None)
+                # # generate a horizontal bar chart using Plotly
+                # fig = px.bar(view_count_df, barmode='group', title="Total Video Minutes - Channel Views After 6th of Feb")
+
+                # fig.update_traces(name="Minutes",selector=dict(name="mins_count_after"))
+
+                # fig.update_traces(name="Views",selector=dict(name="view_count_after"))
+
+                # fig.update_layout(xaxis_title=None,legend=dict(orientation='h',yanchor='top',y=1.1,xanchor='left',x=0.01),legend_title="",
+                # width=800, height=600,yaxis_title=None)
 
                 # display the chart
                 st.plotly_chart(fig)
+
+
 
                 st.markdown("""
 
