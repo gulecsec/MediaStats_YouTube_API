8c04b8f2fcc1229122622590cae86c722e27a520
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-28 18:09:44 +0000

    update

--- app.py
+++ app.py
@@ -309,7 +309,7 @@ Meanwhile, the channels with lower engagement rates could use this information t
 
                 # generate a horizontal bar chart using Plotly
                 fig = px.bar(data_frame=edited_stats_df.sort_values('like_per_video_after', ascending=False),
-                x="channelName", y="like_per_video_after", color='video_count_after')
+                x="channelName", y="like_per_video_after", color='video_count_after', color_continuous_scale='Inferno')
 
                 # fig.update_traces(name="Up to 27/03/23",selector=dict(name="like_per_video_after"))
 
@@ -318,8 +318,6 @@ Meanwhile, the channels with lower engagement rates could use this information t
 
                 # display the chart
                 st.plotly_chart(fig)
-
-
 
                 st.markdown("""
 
