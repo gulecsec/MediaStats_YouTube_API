d7dddbc135378723026b5712eda24cd29bc4d67a
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-28 03:34:38 +0000

    update

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ if page == "Turkish News Media's YouTube Stats":
                 # # generate a horizontal bar chart using Plotly
                 # fig = px.bar(view_count_df, barmode='group', title="Total Video Minutes - Channel Views After 6th of Feb")
 
-                # fig.update_traces(name="Minutes",selector=dict(name="mins_count_after"))
+                fig.update_traces(name="Minutes",selector=dict(name="mins_count_after"))
 
                 # fig.update_traces(name="Views",selector=dict(name="view_count_after"))
 
