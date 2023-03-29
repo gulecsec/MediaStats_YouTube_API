ceec8eeafed6780a89a327e8fc838ca1a917591f
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-29 21:36:53 +0000

    update

--- app.py
+++ app.py
@@ -642,11 +642,11 @@ Overall, the data suggests that there is a considerable variation in the amount 
                 # create a new DataFrame with 'channelName', 'view_per_video_after', and 'view_per_video_before' columns
                 subs_per_video_df = edited_stats_df[['channelName','subscribers', 'subscribers_per_video','totalVideos']]
 
-                fig = px.bar(data_frame=subs_per_video_df.sort_values('subscribers_per_video', ascending=False),
-                          x="channelName", y="subscribers", color='subscribers_per_video')
+                fig = px.bar(data_frame=subs_per_video_df.sort_values('subscribers', ascending=False),
+                x="subscribers", y="channelName", color='subscribers_per_video', orientation='h')
 
-                fig.update_layout(xaxis_title=None,legend=dict(orientation='h',yanchor='top',y=1.1,xanchor='left',x=0.01),legend_title="",
-                width=800, height=600,yaxis_title=None)
+                fig.update_layout(yaxis_title=None, xaxis_title=None,legend=dict(orientation='h',yanchor='top',y=1.1,xanchor='left',x=0.01),legend_title="",
+                width=800, height=600)
 
                 # display the chart
                 st.plotly_chart(fig)
