652723b96c9e72f7ee7f465df5797bea06bcf23b
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-30 01:38:17 +0000

    update

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if page == "Home":
     with header:
         st.title("How Turkish News Media's YouTube Stats Stack Up")
         st.subheader("Exploring the Data on My Streamlit App")
-        st.markdown("In this tutorial, we'll explore how to use the YouTube API with Python to retrieve and analyze the statistics of channels on one of the world's largest video-sharing platforms. Specifically, we'll focus on the channels affected by the earthquake that occurred in Turkey on February 6th, 2023. By comparing the statistics of these channels, we can gain insight into the impact of the earthquake on the YouTube community and learn how to use the YouTube API to automate various tasks related to channel analysis.")
+        st.markdown("In this tutorial, we'll explore how to use the YouTube API with Python to retrieve and analyze the statistics of channels on one of the world's largest video-sharing platforms. Specifically, we'll focus on 15 channels affected by the earthquake that occurred in Turkey on February 6th, 2023. By comparing the statistics of these channels, we can gain insight into the impact of the earthquake on the YouTube community and learn how to use the YouTube API to automate various tasks related to channel analysis.")
 
 
     with dataset:
