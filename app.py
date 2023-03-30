105b2e83d8f6a5a6644c5cfaacb301ebafd2ccb2
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-30 00:18:44 +0000

    update

--- app.py
+++ app.py
@@ -146,8 +146,8 @@ if page == "Turkish News Media's YouTube Stats":
         edited_stats_df = pd.read_csv("All_stats/media_stats_edited.csv")
 
         # Define sidebar
-        st.sidebar.title("Detailed Stats of the Channels")
-        page = st.sidebar.radio("Go to", ("Views-Minutes After", "Likes-Minutes After", "Comments-Minutes After",
+        st.sidebar.title("Detailed Stats")
+        page = st.sidebar.radio("", ("Views-Minutes After", "Likes-Minutes After", "Comments-Minutes After",
         "Likes per Video", "Views per Video", "Comments per Video", "Durations per Video", "Views per Likes", "Number of Videos After",
         "Monthly Minutes After", "Monthly Likes After", "Monthly Views After", "Monthly Comments After","Daily Uploaded Video","Subscribers per Video"))
 
