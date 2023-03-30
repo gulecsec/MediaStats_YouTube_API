f993432df3147a0ab8dc735d46c8ff64f328ff12
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-30 19:11:13 +0000

    update

--- app.py
+++ app.py
@@ -128,8 +128,9 @@ def get_channel_stats(youtube, channel_ids):
 all_data = []
 request = youtube.channels().list(
     part="snippet,contentDetails,statistics",
-    id=','.join(channel_ids)
-)
+    id=','.join(channel_ids))
+
+
 response = request.execute()
 
 # loop through items
