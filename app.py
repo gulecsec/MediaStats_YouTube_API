09ab37653a3566ed1e1af48fc205d0164e3febd7
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-30 19:01:09 +0000

    update

--- app.py
+++ app.py
@@ -51,9 +51,20 @@ if page == "Home":
     if not state_dict[page]["headline_displayed"]:
         st.title("How Turkish News Media's YouTube Stats Stack Up")
         st.subheader("Exploring the Data on My Streamlit App")
-        st.markdown("In this tutorial, we'll explore how to use the YouTube API with Python to retrieve and analyze the statistics of channels on one of the world's largest video-sharing platforms. Specifically, we'll focus on 15 channels affected by the earthquake that occurred in Turkey on February 6th, 2023. By comparing the statistics of these channels, we can gain insight into the impact of the earthquake on the YouTube community and learn how to use the YouTube API to automate various tasks related to channel analysis.")
-        state_dict[page]["headline_displayed"] = True
+        st.markdown("""
+        In this tutorial, we'll explore how to use the YouTube API with Python to retrieve and analyze
 
+        the statistics of channels on one of the world's largest video-sharing platforms. Specifically,
+
+        we'll focus on 15 channels affected by the earthquake that occurred in Turkey on February 6th, 2023.
+
+        By comparing the statistics of these channels, we can gain insight into the impact of the earthquake
+
+        on the YouTube community and learn how to use the YouTube API to automate various tasks related to channel
+
+        analysis.""")
+
+        state_dict[page]["headline_displayed"] = True
 
 
     # add radio button to select between the two graphs
