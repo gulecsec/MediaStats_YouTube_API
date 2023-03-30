ccd8178967a80c075ad456013fdd4ac8869d6a83
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-30 01:30:39 +0000

    update

--- app.py
+++ app.py
@@ -76,9 +76,13 @@ if page == "Google Developers Console":
         st.markdown("""
                 Now that we have the API key and the library installed, we can start making API requests. Here’s a simple example that retrieves the details of a video:
                 """)
-        st.code("from googleapiclient.discovery import build")
-        st.code("api_service_name = 'youtube'")
-        st.code("api_version = 'v3'")
+        st.code("""
+        from googleapiclient.discovery import build
+        api_service_name = 'youtube'
+        api_version = 'v3'
+
+        """)
+
 
 
         st.subheader("Step 5: Get credentials and create an API client")
