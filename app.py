9aaa1ea471af1f13d73e3c1cab3a6b799859a9ee
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-03-30 21:24:35 +0000

    UPDATE

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-from wordcloud import WordCloud
-from nltk.corpus import stopwords
+from wordcloud import WordCloud, STOPWORDS
+
 
 # Data viz packages
 import plotly.express as px
