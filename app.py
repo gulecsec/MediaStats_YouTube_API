8922a04af469f07ec8dd736f99459599f2cd5b41
Selcan Gulec <102465959+gulecsec@users.noreply.github.com>
2023-04-02 18:16:00 +0000

    update

--- app.py
+++ app.py
@@ -84,26 +84,51 @@ if page == "Home":
 
     """)
 
-    st.footer("""
-    <section class="split contact">
-        <section class="alt">
-            <h3>Location</h3>
-            <p>Mexico City, MX</p>
-        </section>
+    ft = """
+<style>
+a:link , a:visited{
+color: #BFBFBF;  /* theme's text color hex code at 75 percent brightness*/
+background-color: transparent;
+text-decoration: none;
+}
 
-        <section>
-            <h3>Email</h3>
-            <p><a href="#">selcangulec@proton.me</a></p>
-        </section>
-        <section>
-            <h3>Social</h3>
-            <ul class="icons alt">
-                <li><a href="https://www.linkedin.com/in/sgulec/" class="icon brands alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
-                <li><a href="https://github.com/gulecsec" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
-            </ul>
-        </section>
-    </section>
-""")
+a:hover,  a:active {
+color: #0283C3; /* theme's primary color*/
+background-color: transparent;
+text-decoration: underline;
+}
+
+#page-container {
+  position: relative;
+  min-height: 10vh;
+}
+
+footer{
+    visibility:hidden;
+}
+
+.footer {
+position: relative;
+left: 0;
+top:230px;
+bottom: 0;
+width: 100%;
+background-color: transparent;
+color: #808080; /* theme's text color hex code at 50 percent brightness*/
+text-align: left; /* you can replace 'left' with 'center' or 'right' if you want*/
+}
+</style>
+
+<div id="page-container">
+
+<div class="footer">
+<p style='font-size: 0.875em;'>Made with <a style='display: inline; text-align: left;' href="https://streamlit.io/" target="_blank">Streamlit</a><br 'style= top:3px;'>
+with <img src="https://em-content.zobj.net/source/skype/289/red-heart_2764-fe0f.png" alt="heart" height= "10"/><a style='display: inline; text-align: left;' href="https://github.com/sape94" target="_blank"> by sape94</a></p>
+</div>
+
+</div>
+"""
+st.write(ft, unsafe_allow_html=True)
 
 if page == "Google Developers Console":
 
